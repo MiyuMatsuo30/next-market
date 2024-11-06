1ddfac7f24c2d8339875c5fde271bd34ea358eaf
MiyuMatsuo30 <matsuo30m@gmail.com>
2024-11-07 01:33:34 +0900

    modified

--- app/item/create/page.js
+++ app/item/create/page.js
@@ -9,24 +9,16 @@ const CreateItem = () => {
     const [newItem, setNewItem] = useState({
         title: "",
         price: "",
-        image: image,
         description: "",
     })
     const router = useRouter()
     const loginUserEmail = useAuth()
     console.log(loginUserEmail)
     const handleChange = (e) => {
-        if (e.target.name === "image") {
-            setNewItem({
-                ...newItem,
-                [e.target.name]: image,
-            })
-        } else {
-            setNewItem({
-                ...newItem,
-                [e.target.name]: e.target.value,
-            })
-        }
+        setNewItem({
+            ...newItem,
+            [e.target.name]: e.target.value,
+        })
     }
     
     const handleSubmit = async(e) => {
@@ -41,7 +33,8 @@ const CreateItem = () => {
                 },
                 body: JSON.stringify({
                     ...newItem,
-                    email: loginUserEmail
+                    email: loginUserEmail,
+                    image: image,
                 })
             })
             const jsonData = await response.json()
@@ -60,7 +53,7 @@ const CreateItem = () => {
                 <form onSubmit={handleSubmit}>
                     <input value={newItem.title} onChange={handleChange} type="text" name="title" placeholder="アイテム名" required />
                     <input value={newItem.price} onChange={handleChange} type="text" name="price" placeholder="価格" required />
-                    <input value={newItem.image} onChange={handleChange} type="text" name="image" placeholder="画像" required />
+                    <input value={image} onChange={(e) => setImage(e.target.value)} type="text" name="image" placeholder="画像" required />
                     <textarea value={newItem.description} onChange={handleChange} type="text" name="description" rows={15} placeholder="商品説明" required></textarea>
                     <button>作成</button>
                 </form>
