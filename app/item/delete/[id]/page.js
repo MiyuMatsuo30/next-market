499b8e420b274e9ed64eaa8d3f19a34da05bdf4c
MiyuMatsuo30 <matsuo30m@gmail.com>
2024-11-06 13:31:30 +0900

    item delete update

--- app/item/delete/[id]/page.js
+++ app/item/delete/[id]/page.js
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react"
 import { useRouter } from "next/navigation"
 import Image from "next/image"
+import useAuth from "../../utils/useAuth"
 
 const DeleteItem = (context) => {
     const [selectedItem, setSelectedItem] = useState({
@@ -12,6 +13,7 @@ const DeleteItem = (context) => {
         email: ""
     })
     const router = useRouter()
+    const loginUserEmail = useAuth()
     useEffect(() => {
         const getSingleItem = async(id) => {
             const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/item/readsingle/${id}`, {cache: "no-store"})
@@ -27,12 +29,6 @@ const DeleteItem = (context) => {
         }
         getSingleItem(context.params.id)
     }, [context])
-    const handleChange = (e) => {
-        setSelectedItem({
-            ...selectedItem,
-            [e.target.name]: e.target.value,
-        })
-    }
     const handleSubmit = async(e) => {
         e.preventDefault()
         try {
@@ -45,7 +41,7 @@ const DeleteItem = (context) => {
                 },
                 body: JSON.stringify({
                     ...selectedItem,
-                    email: "ダミーデータ"
+                    email: loginUserEmail
                 })
             })
             const jsonData = await response.json()
@@ -56,18 +52,22 @@ const DeleteItem = (context) => {
             alert("アイテム削除失敗")
         }
     }
-    return (
-        <div>
-            <h1 className="page-title">アイテム削除</h1>
-            <form onSubmit={handleSubmit}>
-                <h2>{selectedItem.title}</h2>
-                <Image src={selectedItem.image} width={750} height={500} alt="item-image" priority />
-                <h3>¥{selectedItem.price}</h3>
-                <p>{selectedItem.description}</p>
-                <button>削除</button>
-            </form>
-        </div>
-    )
+    if (loginUserEmail === selectedItem.email) {
+        return (
+            <div>
+                <h1 className="page-title">アイテム削除</h1>
+                <form onSubmit={handleSubmit}>
+                    <h2>{selectedItem.title}</h2>
+                    <Image src={selectedItem.image} width={750} height={500} alt="item-image" priority />
+                    <h3>¥{selectedItem.price}</h3>
+                    <p>{selectedItem.description}</p>
+                    <button>削除</button>
+                </form>
+            </div>
+        )
+    } else {
+        <h1>権限がありません</h1>
+    }
 }
 
 export default DeleteItem
